4ee751fb9f1eca65940c9b83f112912f5b146d7f
LoginamNet <loginamnet@gmail.com>
2022-08-09 16:01:45 +0300

    refactor: try to solve what season task

--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  let season;
+
+  function noDate(message) {
+    this.message = `'Invalid date!'`;
+    this.name = noDate;
+  }
+
+  if (!arguments.length) {return 'Unable to determine the time of year!'}
+
+  if (Date.parse(date) === NaN) {throw new noDate('Invalid date!')}
+
+  let month = date.getMonth();
+
+  if (month < 2 || month  === 11) {
+    season = `winter`;
+  } else if (month >= 2 && month < 5) {
+    season = `spring`;
+  } else if (month >= 5 && month < 8) {
+    season = `summer`;
+  } else if (month >= 8 && month < 11) {
+    season = `autumn`;
+  }
+
+  return season;
   // remove line with error and write your code here
 }
 
