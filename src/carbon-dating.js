bf8a846a390d02af069f50f03b25e6603f8d5867
LoginamNet <loginamnet@gmail.com>
2022-08-11 08:20:55 +0300

    feat: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,7 +18,7 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  return ((sampleActivity instanceof String) && sampleActivity > 0 && sampleActivity < 15) ? Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)) : false
+  return (sampleActivity > 0 && sampleActivity < 15 &&  typeof sampleActivity === `string`) ? Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)) : false
   // remove line with error and write your code here
 }
 
