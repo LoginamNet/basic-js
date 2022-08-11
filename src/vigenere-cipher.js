6a7485efb63ea8b9d4052fecb2c07df743b01401
LoginamNet <loginamnet@gmail.com>
2022-08-11 11:01:48 +0300

    feat: vigenere ciper solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,16 +19,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+ class VigenereCipheringMachine {
+
+  constructor(reverse = true) {
+    this.reverse = reverse;
+  };
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error(`Incorrect arguments!`);
+
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+
+    while (key.length <= message.length) {
+      key += key;
+    };
+
+    for (let i = 0; i < message.length; i++) {
+
+      if (message[i].match(/[A-Z]/gm)) {
+        let addr = ((alphabet.length + alphabet.search(message[i]) + alphabet.search(key[i])) % alphabet.length);
+        result += alphabet[addr % alphabet.length];
+      } else {
+        result += message[i];
+        key = key.slice(0, i) + message[i] + key.slice(i);
+      }
+    }
+
+    return this.reverse ? result : result.split('').reverse().join('');
+  };
+  decrypt(message, key) {
+      if (!message || !key) throw new Error(`Incorrect arguments!`);
+
+      key = key.toUpperCase();
+      message = message.toUpperCase();
+      let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let result = '';
+  
+  
+      while (key.length <= message.length) {
+        key += key;
+      };
+  
+      for (let i = 0; i < message.length; i++) {
+  
+        if (message[i].match(/[A-Z]/gm)) {
+          let addr = ((alphabet.length + alphabet.search(message[i]) - alphabet.search(key[i])) % alphabet.length);
+          result += alphabet[addr % alphabet.length];
+        } else {
+          result += message[i];
+          key = key.slice(0, i) + message[i] + key.slice(i);
+        }
+      }
+  
+      return this.reverse ? result : result.split('').reverse().join('');
+  };
+};
 
 module.exports = {
   VigenereCipheringMachine
