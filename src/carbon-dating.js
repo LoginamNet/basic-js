32390dc372d2c5fd30fe7303fb91be7bd3e31571
LoginamNet <loginamnet@gmail.com>
2022-08-11 08:19:35 +0300

    init: stard carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,8 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  return ((sampleActivity instanceof String) && sampleActivity > 0 && sampleActivity < 15) ? Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)) : false
   // remove line with error and write your code here
 }
 
