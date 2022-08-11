19134d520788b4c2cc1baca2348d861ed322bd88
LoginamNet <loginamnet@gmail.com>
2022-08-11 10:04:14 +0300

    feat: add emcode line solution

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,8 +10,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+ function encodeLine(str) {
+
+  let encodeLine = [];
+  let letterAmount = 1;
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (str[i] === str[i + 1]) {
+      letterAmount++;
+    } else {
+    
+      (letterAmount === 1) ? encodeLine.push(`${str[i]}`) : encodeLine.push(`${letterAmount}${str[i]}`);
+      letterAmount = 1;
+    }
+
+  }
+  
+  console.log(encodeLine);
+  return encodeLine.join('');
   // remove line with error and write your code here
 }
 
